4691393fbe37fce1a65d8d0926dfa8910d13ecb5
swasher <mr.swasher@gmail.com>
2019-05-26 17:14:28 +0300

    init

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask import jsonify
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
+from .models import Effect, Ingredient
+
+app = Flask(__name__)
+
+# admin -----------------------
+app.config['FLASK_ADMIN_SWATCH'] = os.getenv("FLASK_ADMIN_SWATCH")
+admin = Admin(app, name='Skyrim Alchemy', template_mode='bootstrap3')
+admin.add_view(ModelView(Effect))
+admin.add_view(ModelView(Ingredient))
+# admin -----------------------
+
+
+@app.route('/ingredients', methods=['GET'])
+def main():
+    query = Ingredient.select().dicts()
+    name = "Swasher"
+    # return render_template('index.html', d=d)
+    return jsonify({'ingredients': list(query)})
+
+
+@app.route('/')
+def index():
+    s = os.getenv("SECRET_KEY")
+    return f'Hello! flask_env={s}'
---
+++ app/models.py
@@ -0,0 +1,57 @@
+import os
+from peewee import *
+
+FLASK_ENV = os.getenv("FLASK_ENV")
+
+
+database_proxy = DatabaseProxy()
+
+
+class Effect(Model):
+    name = CharField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        database = database_proxy  # This model uses the "people.db" database.
+
+
+class Ingredient(Model):
+    name = CharField(unique=True)
+    effect1 = ForeignKeyField(Effect)
+    effect2 = ForeignKeyField(Effect)
+    effect3 = ForeignKeyField(Effect)
+    effect4 = ForeignKeyField(Effect)
+
+    class Meta:
+        database = database_proxy  # This model uses the "people.db" database.
+
+# db = SqliteDatabase('base.db')
+# def initialize_db():
+#     db.connect()
+#     db.create_tables([Effect], safe=True)
+#     db.create_tables([Ingredient], safe=True)
+#     db.close()
+# initialize_db()
+
+
+# Based on configuration, use a different database.
+if FLASK_ENV == 'development':
+    database = SqliteDatabase('base.db')
+elif FLASK_ENV == 'production':
+    database = PostgresqlDatabase('my_app', user='postgres', password='secret', host='10.1.0.9', port=5432)
+else:
+    raise Exception('No FLASK_ENV environment during db init.')
+
+
+# Configure our proxy to use the db we specified in config.
+database_proxy.initialize(database)
+
+
+def initialize_db():
+    database_proxy.connect()
+    database_proxy.create_tables([Effect], safe=True)
+    database_proxy.create_tables([Ingredient], safe=True)
+    database_proxy.close()
+initialize_db()
